0b1efe25c6d21a24adc5bfa307c395b0294f841d
Champa Jha <champajha28@gmail.com>
2026-05-25 08:16:45 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/firebase/client-provider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useMemo, ReactNode } from 'react';
+import { initializeFirebase } from './index';
+import { FirebaseProvider } from './provider';
+
+export function FirebaseClientProvider({ children }: { children: ReactNode }) {
+  const { app, db, auth } = useMemo(() => initializeFirebase(), []);
+
+  return (
+    <FirebaseProvider app={app} db={db} auth={auth}>
+      {children}
+    </FirebaseProvider>
+  );
+}
